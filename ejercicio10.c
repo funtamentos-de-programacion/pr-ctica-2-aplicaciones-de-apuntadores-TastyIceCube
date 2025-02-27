82dfb0a8888ae2a6b383205ed5857c98bac35fb4
Daniel salinas <danielsalinas544@gmail.com>
2025-02-26 22:04:00 -0600

    avance 2

--- ejercicio10.c
+++ ejercicio10.c
@@ -2,21 +2,30 @@
 #include <stdio.h>
 
 void ordenarBurbuja(int *arr, int tamaño) {
-    for (int i = 1; i < tamaño; i++)
-        for (int j = 0; i < tamaño; i++)
-        {
-            if (arr[j] > arr[i]);
-        }
-        
-    {
-        /* code */
-    }
-    
     // Implementar algoritmo de ordenamiento aquí
+    for (int i = 0; i < tamaño - 1; i++) {
+        for (int j = 0; j < tamaño - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Intercambiar elementos usando apuntadores
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
 
 int main() {
     int arr[] = {34, 12, 56, 78, 23};
+    int tamaño = sizeof(arr) / sizeof(arr[0]);
+
     // Código para ordenar y mostrar el arreglo
+    ordenarBurbuja(arr, tamaño);
+    printf("Arreglo ordenado: ");
+    for (int i = 0; i < tamaño; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
     return 0;
 }
